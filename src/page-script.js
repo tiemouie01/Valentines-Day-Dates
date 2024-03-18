211b369a0c8f8fffab81255094b7ea32d6390dbf
Timothy Miamba <timal0361@gmail.com>
2024-03-18 17:58:44 +0200

    Change page layouts to enable hiding functionality

--- src/page-script.js
+++
@@ -1,9 +0,0 @@
-const hideUi = function removeUiFromScreen() {
-  const homeBar = document.querySelector(".home-bar");
-  homeBar.addEventListener("click", () => {
-    const phone = document.getElementById("phone");
-    phone.classList.toggle("hide");
-  });
-};
-
-hideUi();
