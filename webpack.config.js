529d6c98fdf786a8223b99280285f7aac9f8ed2b
Timothy Miamba <timal0361@gmail.com>
2024-03-21 17:51:47 +0200

    Update webpack config for iframes

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
